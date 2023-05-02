6b89078bb94420f6cf14dde12fda9b04349224ad
sangeetha2521 <sangeens8101@gmail.com>
2023-05-02 22:21:49 +0530

    Environment setup completed

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>It's my first react native app! wohooo!</Text>
       <StatusBar style="auto" />
     </View>
   );
